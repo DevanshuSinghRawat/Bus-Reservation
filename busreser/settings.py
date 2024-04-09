9cdb7768c6094ae6c3dbad05aaf61deed71ec1ad
Devanshu Singh Rawat <devanshu21001@gmail.com>
2024-04-09 15:14:23 +0530

    initial commit

--- busreser/settings.py
+++ busreser/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-if&sa=)mzx7^s8ym@eq(*%(^qc&&zk-g8#=jdu$qdz)fa-l-7q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False # default was True (changed for deployment)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
